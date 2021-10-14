ef47ecbbeb7ac328495de431049954039c9ac73d
Dominic Altamura <daltamur@oswego.edu>
2021-10-14 14:40:25 -0400

    Initial Start up connection modified

---
+++ pythonServer/interpreter.py
@@ -0,0 +1,15 @@
+def get_clock(values):
+    return values[0]
+
+
+def get_bus(values):
+    return values[1]
+
+
+def get_mc(values):
+    return values[2]
+
+
+def interpret(vals_array):
+    get_mc(vals_array)
+    print(vals_array)
